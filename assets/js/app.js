88b7d141d3e149a3d28ebd75a7c5e12d3ea9f9a3
raphael-morales <raphael.morales@hotmail.fr>
2020-07-22 20:43:36 +0200

    mise en forme de la page d'accueil

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,21 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// app.js
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
 // any CSS you import will output into a single scss file (app.scss in this case)
 import '../scss/app.scss';
 
