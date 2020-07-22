670d040a0b3d700058c0dbd851658df1d14fae56
raphael-morales <raphael.morales@hotmail.fr>
2020-07-22 16:22:13 +0200

    modification homeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @return Response
      */
     public function index() :Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
