ec514623a9878001bf21f9fea05f4aad2136d6fe
raphael-morales <raphael.morales@hotmail.fr>
2020-07-22 12:34:51 +0200

    creation de l'entité reservation

---
+++ src/Entity/Book.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $numberOfPersons;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumberOfPersons(): ?int
+    {
+        return $this->numberOfPersons;
+    }
+
+    public function setNumberOfPersons(int $numberOfPersons): self
+    {
+        $this->numberOfPersons = $numberOfPersons;
+
+        return $this;
+    }
+}
